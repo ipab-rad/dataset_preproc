549b35a00e6856d363fe411b0c6b866748a81284
hect95 <hector_cruz95@live.com>
2025-03-11 17:17:23 +0000

    Rename `add_3d_sample` to `add_segmentsai_sample`
    - Update `pyproject.toml` with the new script command
    - Update README

--- scripts/labelling_preproc/add_segmentsai_sample.py
+++ scripts/labelling_preproc/add_segmentsai_sample.py
@@ -141,7 +141,7 @@ def main():
     if len(sys.argv) < 4:
         print(
             'ERROR: Please provide the required arguments\n'
-            'add_3d_sample <dataset_name> <sequence_name> <data_directory>',
+            'add_segmentsai_sample <dataset_name> <sequence_name> <data_directory>',
             file=sys.stderr,
         )
         sys.exit(1)
